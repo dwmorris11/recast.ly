7ae99e85cfb29e8cfaccf49cdc8753f274b9c133
Jim Knape <Jimwinthropknape@gmail.com>
2020-05-27 16:34:11 -0700

    Refactored Search as a stateful class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,47 @@
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import searchYouTube from '../lib/searchYouTube.js';
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       done: false,
       videos: exampleVideoData,
-      currentPlaying: exampleVideoData[0]
+      currentPlaying: exampleVideoData[0],
+      request: ''
     };
     this.handleClick = this.handleClick.bind(this);
+    this.search = this.search.bind(this);
   }
   handleClick(index) {
     this.setState ({
       currentPlaying: exampleVideoData[index]
     });
   }
+  searchState(videos) {
+    this.setState ({
+      videos: videos,
+      currentPlaying: videos[0]
+    });
+  }
+
+  search(apiKey, request) {
+    var options = {
+      key: apiKey,
+      query: request,
+      max: 5
+    };
+    searchYouTube(options, (data) => (this.searchState(data)))
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search searchFunction={this.search}/>
           </div>
         </nav>
         <div className="row">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,29 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+import YOUTUBE_API_KEY from "../config/youtube.js";
+class Search extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState ({
+      value: event.target.value
+    });
+  }
+
+  render() {
+    return (
+    <div className="search-bar form-inline">
+      <input className="form-control" value={this.state.value} onChange={this.handleChange} type="text" id="request"/>
+      <button className="btn hidden-sm-down" onClick={()=>(props.searchFunction(YOUTUBE_API_KEY, this.state.value))}>
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div>
+    )
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
