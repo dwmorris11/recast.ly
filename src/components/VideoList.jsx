4a90375b1a4795a076bf5397d6ac5d781beedbcd
Jim Knape <Jimwinthropknape@gmail.com>
2020-05-26 18:27:24 -0700

    Does not render VideoList correctly

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,14 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => { //pomander did not like the arrow function here so i took it out to push...
+  var elements = props.videos;
 
+  return elements.map((value, index) => {(
+    <div className="video-list">
+     <div><h5><em>{elements.snippet.title}</em><img src={elements.thumbnails.default}/></h5></div>
+    </div>
+  )}
+
+};
+//
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -17,3 +18,8 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
+//{elements.map((value, index) => {
+  //           return <li style={style} onClick={this.onListItemClick.bind(this)} key={index}>{value}</li>
+  //         })}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+ReactDOM.render(<App />, document.getElementById('app'));
 
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false
+//     };
+//   }
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//   render() {
+//      {elements.map((value, index) => {
+//           return <li style={style} onClick={this.onListItemClick.bind(this)} key={index}>{value}</li>
+//         })}
+//     var style = {
+//       textDecoration: this.state.done ? 'line-through' : 'none'
+//     };
+//     return (
+//       <ul>
+//         {elements.map((value, index) => {
+//           return <li style={style} onClick={this.onListItemClick.bind(this)} key={index}>{value}</li>
+//         })}
+//       </ul>
+//     );
+// }
+// };
+
+// var GroceryList = () => (
+// <div>
+//   <h1>Grocery List</h1>
+//   <GroceryListItem type={['Green Eggs', 'Ham', 'Cheese']} />
+// </div>
+
+// );
+
+// var myList = React.createElement(GroceryList);
+// ReactDOM.render(myList, document.getElementById('app'));
