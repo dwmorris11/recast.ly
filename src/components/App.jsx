ab54edeaa18b9fa2a331bf5c15016127b6f72651
Dustin Morris <dustinnuc@gmail.com>
2020-05-26 22:19:38 -0500

    VideoList displaying title and pictures as a functional react component. Needs formatting.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <VideoList videos={exampleVideoData}><h5><em>videoList</em> view goes here</h5></VideoList>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,12 @@
 var VideoList = (props) => { //pomander did not like the arrow function here so i took it out to push...
   var elements = props.videos;
 
-  return elements.map((value, index) => {(
-    <div className="video-list">
-     <div><h5><em>{elements.snippet.title}</em><img src={elements.thumbnails.default}/></h5></div>
+  var output = elements.map((value, index, array) => (
+    <div className="video-list" key={index}>
+      <div ><h5><em>{array[index].snippet.title}</em><img src={array[index].snippet.thumbnails.default.url}/></h5></div>
     </div>
-  )});
+  ));
+  return output;
 };
 //
 // PropTypes tell other developers what `props` a component expects
@@ -17,8 +18,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
-
-
-//{elements.map((value, index) => {
-  //           return <li style={style} onClick={this.onListItemClick.bind(this)} key={index}>{value}</li>
-  //         })}
